14b9ad0e9994f7674ad0588231cef738b5344d6f
azanexpl <51541101+azanexpl@users.noreply.github.com>
2019-06-21 23:44:50 +0200

    Klasa zawierajaca parametry jedzenia

---
+++ Jedzenie.java
@@ -0,0 +1,14 @@
+package application;
+
+/**
+ * Klasa zawierajaca parametry 'jedzenia' wystepujacego na planszy takie jak: pozycjaX, pozycjaY oraz kolor
+ * @author azane
+ *
+ */
+public class Jedzenie {
+
+	static int kolorjedzenia = 0;
+	static int jedzenieX = 0;
+	static int jedzenieY = 0;
+
+}
