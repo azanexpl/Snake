96e367ccb331729ef4fed180bd429f81943bfad4
azanexpl <51541101+azanexpl@users.noreply.github.com>
2019-06-21 23:46:04 +0200

    Klasa zawierajaca parametry segmentow z ktorych sklada sie waz

---
+++ Segment.java
@@ -0,0 +1,19 @@
+package application;
+
+/**
+ * Klasa zawierajaca parametry segmentow z ktorych sklada sie waz
+ * @author azane
+ *
+ */
+public class Segment {
+
+	int x;
+	int y;
+	static int rozmiarSegmentu = 25;
+
+	Segment(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+}
