ec3728e7281422b9658f1e11791e1d33efd52d68
azanexpl <51541101+azanexpl@users.noreply.github.com>
2019-06-21 23:45:08 +0200

    Klasa enum zawierajaca kierunki poruszania sie weza

---
+++ Kierunek.java
@@ -0,0 +1,10 @@
+package application;
+
+/**
+ * Klasa typu enum zawieraj¹ca kierunki poruszania sie weza: GORA, DOL, LEWO, PRAWO
+ * @author azane
+ *
+ */
+public enum Kierunek {
+	GORA, DOL, PRAWO, LEWO
+}
